0dab0ff319e149398b4a431d0ae4f9620715853b
steveasterafovo <steveasterafovo@gmail.com>
2026-08-18 21:58:52 +0100

    s

--- src/components/pages/docs/index.ts
+++ src/components/pages/docs/index.ts
@@ -2,3 +2,4 @@ export { ArticleMeta } from "./ArticleMeta";
 export { ArticlePaginationNav } from "./ArticlePaginationNav";
 export { DocsBreadcrumb } from "./DocsBreadcrumb";
 export { DesktopSidebar, MobileSidebar } from "./SidebarContainer";
+export { DocsViewer } from "./DocsViewer";
