bb4857c7c1a03cc720a219fc283a7e27d94586a7
steveasterafovo <steveasterafovo@gmail.com>
2026-08-18 22:37:39 +0100

    d

---
+++ src/components/pages/support/home/index.ts
@@ -0,0 +1,4 @@
+export { SupportCategories } from "./Categories";
+export { SupportHeader } from "./Header";
+export { SupportPopularAndContact } from "./PopularAndContact";
+export { SupportSearchHero } from "./SearchHero";
