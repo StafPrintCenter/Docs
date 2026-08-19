72aa719a7558dd4e2e9cab3cce4fdb528592cdb9
steveasterafovo <steveasterafovo@gmail.com>
2026-08-19 02:48:16 +0100

    s

--- src/content/docs/student/index.ts
+++ src/content/docs/student/index.ts
@@ -3,12 +3,11 @@ import { group as g0 } from "./inscription";
 import { group as g1 } from "./suivi";
 
 export const space: DocSpace = {
-  id: "student-hub",
-  name: "Student Hub",
+  id: "student",
+  name: "Espace Apprenant",
   shortName: "Student",
   tagline: "Espace des apprenants",
-  description:
-    "S'inscrire à une formation, suivre ses cours, rendre ses devoirs et récupérer ses attestations depuis le Student Hub.",
+  description: "S'inscrire à une formation, suivre ses cours, rendre ses devoirs et récupérer ses attestations depuis le Student Hub.",
   groups: [g0, g1],
 };
 
