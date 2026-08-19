50c620713e310dc17a785135532c3c0e132e7167
steveasterafovo <steveasterafovo@gmail.com>
2026-08-19 02:46:15 +0100

    s

--- src/content/docs/index.ts
+++ src/content/docs/index.ts
@@ -1,7 +1,7 @@
 import type { DocSpace } from "@/types/docs";
 import { space as s0 } from "./landing";
 import { space as s1 } from "./instructor";
-import { space as s2 } from "./student-hub";
+import { space as s2 } from "./student";
 import { space as s3 } from "./spc-meet";
 import { space as s4 } from "./spc-arcade";
 
