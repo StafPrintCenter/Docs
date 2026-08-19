7e7a665de1d82803e8f1627807fd0fffa4ea66d7
steveasterafovo <steveasterafovo@gmail.com>
2026-08-19 02:28:04 +0100

    s

---
+++ src/content/docs/index.ts
@@ -0,0 +1,11 @@
+import type { DocSpace } from "@/types/docs";
+import { space as s0 } from "./site-vitrine";
+import { space as s1 } from "./instructor-hub";
+import { space as s2 } from "./student-hub";
+import { space as s3 } from "./spc-meet";
+import { space as s4 } from "./spc-arcade";
+
+
+export const spaces: DocSpace[] = [s0, s1, s2, s3, s4];
+
+export default spaces;
