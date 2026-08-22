8464e5a24b868ab015057ee89e6cb2e70fa6d65e
steveasterafovo <steveasterafovo@gmail.com>
2026-08-22 13:58:24 +0100

    s

---
+++ src/content/support/3.opinion/index.ts
@@ -0,0 +1,10 @@
+import type { SupportCategory } from "@/types/support";
+
+export const category: SupportCategory = {
+  id: "opinion",
+  title: "Projets, Devis & Avis clients",
+  description: "Liste des outils.",
+  icon: "account",
+};
+
+export default { category };
