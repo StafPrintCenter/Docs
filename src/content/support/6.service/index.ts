fd4b561e1c7bd76b39431b6a83e45688e3593f01
steveasterafovo <steveasterafovo@gmail.com>
2026-08-22 14:47:21 +0100

    s

--- src/content/support/6.service/index.ts
+++ src/content/support/6.service/index.ts
@@ -1,7 +1,7 @@
 import type { SupportCategory } from "@/types/support";
 
 export const category: SupportCategory = {
-  id: "tarining",
+  id: "services",
   title: "Services, Production & Support Technique",
   description: "Liste des outils.",
   icon: "account",
