fc2e2cd0c4ee359a60235e52dc588c169a81349c
steveasterafovo <steveasterafovo@gmail.com>
2026-08-22 13:04:35 +0100

    s

--- src/components/pages/docs/index.ts
+++ src/components/pages/docs/index.ts
@@ -2,4 +2,5 @@ export { ArticleMeta } from "./ArticleMeta";
 export { ArticlePaginationNav } from "./ArticlePaginationNav";
 export { DocsBreadcrumb } from "./DocsBreadcrumb";
 export { DesktopSidebar, MobileSidebar } from "./SidebarContainer";
+export { ArticleContent } from "./ArticleContent";
 export { DocsViewer } from "./DocsViewer";
