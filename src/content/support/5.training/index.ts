413e95012c3954713c256f3a62d989b53e5c3b9a
steveasterafovo <steveasterafovo@gmail.com>
2026-08-22 14:56:52 +0100

    s

--- src/content/support/5.training/index.ts
+++ src/content/support/5.training/index.ts
@@ -3,8 +3,8 @@ import type { SupportCategory } from "@/types/support";
 export const category: SupportCategory = {
   id: "5.training",
   title: "Formations & Compétences",
-  description: "Liste des outils.",
-  icon: "account",
+  description: "Programmes de formation en prépresse, design graphique et développement web.",
+  icon: "training",
 };
 
 export default { category };
