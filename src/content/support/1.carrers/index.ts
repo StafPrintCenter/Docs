0d8fcd951a2ae4f0849ecfee84ffd533e63647cf
steveasterafovo <steveasterafovo@gmail.com>
2026-08-22 14:55:01 +0100

    s

--- src/content/support/1.carrers/index.ts
+++ src/content/support/1.carrers/index.ts
@@ -3,8 +3,8 @@ import type { SupportCategory } from "@/types/support";
 export const category: SupportCategory = {
   id: "carrers",
   title: "Recrutement & Candidatures",
-  description: "Postuler et suivre sa candidature.",
-  icon: "account",
+  description: "Processus d'embauche, offres d'emploi, candidatures spontanées et stages.",
+  icon: "careers",
 };
 
 export default { category };
