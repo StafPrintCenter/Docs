28200fa4d950ebe610ccfa9e2cd704bb223246dc
steveasterafovo <steveasterafovo@gmail.com>
2026-08-31 05:30:32 +0100

    s

--- src/components/pages/home/index.ts
+++ src/components/pages/home/index.ts
@@ -1,3 +1,4 @@
 export { DocPreviewIllustration } from "./DocPreviewIllustration";
 export { HeroSection } from "./HeroSection";
+export { SpacesSectionSkeleton } from "./Skeleton";
 export { SpacesSection } from "./SpacesSection";
