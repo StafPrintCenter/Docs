7910850d09e79db138bd3c99c8c32c6e9252c707
steveasterafovo <steveasterafovo@gmail.com>
2026-09-07 11:50:12 +0100

    z

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -76,7 +76,7 @@ function RootComponent() {
     <QueryClientProvider client={queryClient}>
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
-      <Toaster richColors position="top-right" />
+      <Toaster richColors position="top-right" closeButton />
     </QueryClientProvider>
   );
 }
